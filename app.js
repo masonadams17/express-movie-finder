9d07ff9463e0f011bd3cc7bafa6cce91b6d72b32
adl585 <adl585@my.utsa.edu>
2021-05-11 22:53:50 -0500

    changed the port

--- app.js
+++ app.js
@@ -87,6 +87,6 @@ process.on('exit', function(){
 })
 
 // Start Server
-app.listen(3000, function(){
+app.listen(process.env.PORT || 3000, function(){
     console.log('Server started on port 3000 ... ');
 });
